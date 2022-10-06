1874559fc7a6f2339138c3449d791cb3301f21c7
Sivananthanathan Apisathan <88615588+apisathan2000@users.noreply.github.com>
2022-10-06 16:20:36 +0530

    Add files via upload

--- loader.s
+++ loader.s
@@ -14,4 +14,4 @@
     loader:                         ; the loader label (defined as entry point in linker script)
         mov eax, 0xCAFEBABE         ; place the number 0xCAFEBABE in the register eax
     .loop:
-        jmp .loop 
+        jmp .loop                   ; loop forever
