8a0fa5ce4810bb064b8bb8e2377132cd43fa078b
Sivananthanathan Apisathan <88615588+apisathan2000@users.noreply.github.com>
2022-10-07 14:47:46 +0530

    Add files via upload

--- loader.s
+++ loader.s
@@ -15,3 +15,21 @@
         mov eax, 0xCAFEBABE         ; place the number 0xCAFEBABE in the register eax
     .loop:
         jmp .loop                   ; loop forever
+
+ KERNEL_STACK_SIZE equ 4096                  ; size of stack in bytes
+
+    section .bss
+    align 4                                     ; align at 4 bytes
+    kernel_stack:                               ; label points to beginning of memory
+        resb KERNEL_STACK_SIZE                  ; reserve stack for the kernel
+
+mov esp, kernel_stack + KERNEL_STACK_SIZE
+
+extern sum_of_three   ; the function sum_of_three is defined elsewhere
+
+push dword 3            ; arg3
+push dword 2            ; arg2
+push dword 1            ; arg1
+call sum_of_three       ; call the function, the result will be in eax
+
+
